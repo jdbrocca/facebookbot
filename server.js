9d038d034b4142cf09dfb80e5656ea1f9a92b5ed
Juan Diego <jdbrocca@gmail.com>
2016-08-23 20:27:23 +0000

    Facebook bot

--- server.js
+++ server.js
@@ -71,13 +71,11 @@ function receivedMessage(event) {
 
   var messageId = message.mid;
 
-  console.log("JUan diego: " + message.text)
   // You may get a text or attachment but not both
   var messageText = message.text;
   var messageAttachments = message.attachments;
 
   if (messageText) {
-    console.log("Message Text: " + messageText)
     // If we receive a text message, check to see if it matches any special
     // keywords and send back the corresponding example. Otherwise, just echo
     // the text we received.
@@ -204,6 +202,23 @@ function sendGenericMessage(recipientId) {
   };  
 
   callSendAPI(messageData);
+}
+
+function receivedPostback(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfPostback = event.timestamp;
+
+  // The 'payload' param is a developer-defined field which is set in a postback 
+  // button for Structured Messages. 
+  var payload = event.postback.payload;
+
+  console.log("Received postback for user %d and page %d with payload '%s' " + 
+    "at %d", senderID, recipientID, payload, timeOfPostback);
+
+  // When a postback is called, we'll send a message back to the sender to 
+  // let them know it was successful
+  sendTextMessage(senderID, "Postback called");
 }
 
 app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
