6a7c61ef38f80a4e1501e6698ba7032558ab755f
Juan Diego <jdbrocca@gmail.com>
2016-08-23 20:23:53 +0000

    Facebook bot

--- server.js
+++ server.js
@@ -71,12 +71,13 @@ function receivedMessage(event) {
 
   var messageId = message.mid;
 
+  console.log("JUan diego: " + message.text)
   // You may get a text or attachment but not both
   var messageText = message.text;
   var messageAttachments = message.attachments;
 
   if (messageText) {
-    console.log(messageText)
+    console.log("Message Text: " + messageText)
     // If we receive a text message, check to see if it matches any special
     // keywords and send back the corresponding example. Otherwise, just echo
     // the text we received.
