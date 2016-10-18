7e27c1a2ba4d07edaea20fcfb8b172ab9ee45e94
Juan Diego <jdbrocca@gmail.com>
2016-10-18 17:57:55 +0000

    Cron

--- server.js
+++ server.js
@@ -20,14 +20,15 @@ const
 
 
 var CronJob = require('cron').CronJob;
-var timeZone = 'America/Lima';
-var job = new CronJob('00 56 12 * * *', function() {
+//var timeZone = 'America/Lima';
+var timeZone = 'ACT';
+var job = new CronJob('00 59 12 * * *', function() {
         /*
         * Runs every weekday (Monday through Friday)
         * at 11:30:00 AM. It does not run on Saturday
         * or Sunday.
         */
-        sendTextMessage(1181494861924828, 'Hola JD, que tal?');
+        sendTextMessage(1181494861924828, 'Hola JD v2');
     }, function () {
         /* This function is executed when the job stops */
     },
