fd8e26c77c5e8dc815a92c43c1b3e7d9863c12af
Juan Diego <jdbrocca@gmail.com>
2016-10-18 16:28:22 +0000

    Cron

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const
     request = require('request');
 
 var CronJob = require('cron').CronJob;
-var job = new CronJob('00 26 11 * * *', function() {
+var job = new CronJob('00 30 11 * * *', function() {
         /*
         * Runs every weekday (Monday through Friday)
         * at 11:30:00 AM. It does not run on Saturday
@@ -29,8 +29,7 @@ var job = new CronJob('00 26 11 * * *', function() {
     }, function () {
         /* This function is executed when the job stops */
     },
-    true, /* Start the job right now */
-    timeZone /* Time zone of this job. */
+    true /* Start the job right now */
 );
 
 var app = express();
